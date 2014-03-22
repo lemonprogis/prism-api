75a54750941555ab78849cf6be9d7eb6f8397de0
lemonprogis <edward.briggler@gmail.com>
2014-03-22 01:38:44 -0500

    t

--- local_test.py
+++ local_test.py
@@ -1,15 +1,12 @@
 from PrismAPI.Main import PrismAPI
 
-
-
 if __name__ == '__main__':
     p_id = '62404'
     prism = PrismAPI.PrismAPI()
     prism.set_basic_auth("brigglere","13S!ri-3")
-##    stuff = prism.page_prism(25,25,'summary')
-##    for s in stuff['list']:
-##        print s['emails'][0]['value']
-##
-##    me = prism.me(p_id)
-##    print me['emails'][0]['value']
-    print prism.post_announcement(p_id,"python subject","hello from python!")
+    results = prism.search_people(['programmer'],25,0)
+    s_view = prism.simple_view(results)
+    
+    prism.write_to_csv("s_view.csv",results)
+    
+    
