475fd7c0aefe806bf2bc15464b961af36652a915
unknown <rodkeys@223F7V1.corp.leidos.com>
2015-02-13 14:31:10 -0500

    Moved username/password to local environment variables

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from PrismAPI.Main import PrismAPI
+import os
 
 ################################
 ##  test.py file used for demonstration
@@ -8,14 +9,16 @@ from PrismAPI.Main import PrismAPI
 # log on to prism.leidos.com and edit your profile to see your id...it's in the url (targetUser=12345)
 #p_id = '<user_id>'
 prism = PrismAPI.PrismAPI()
-# jive uses basic http auth
-prism.set_basic_auth("brigglere","13S!ri-3")
+# set your Leidos username/password as environment variables
+PRISM_USERNAME = os.environ.get('PRISM_USERNAME')
+PRISM_PASSWORD = os.environ.get('PRISM_PASSWORD')
+# Jive uses basic http auth
+prism.set_basic_auth(PRISM_USERNAME,PRISM_PASSWORD)
 # searching 
-query = ['programmer']
-results = prism.search_people(query,25,0)
+query = ['President']
+results = prism.search_people(query,100,0)
 
 s_results = prism.simple_view(results) # simple view of jive information
 
 for result in s_results:
     print "%s, %s" % (result["Display Name"], result['Title'])
-
